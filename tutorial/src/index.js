bdf79363b874e02f5aa4b7ff061b90542673f70d
Chih-Chien-Lin <ro0566320@gmail.com>
2021-06-02 00:00:31 -0400

    Nested Component

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,59 +1,25 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+// Nested Components, React Tools
 
-// function is component, return html
-// function name should start with upper case to be recognized as component.
-// Always have to return smth, even empty html tags
-// function Greeting(){
-//   return <h4>hello, test Justin first cmp</h4>;
-// }
-
-// JSX Rules
-// return single element( wrap everything in a div / section / article)
-// div / section / article or Fragment
-// use camelCase for html attribute
-// className instead of class
-// close every element
-// formatting
 function Greeting(){
   return(
-    // <div>
-    // if not use too many <div>, we can use React.Fragment as replacement.
-    <React.Fragment>
-      <h3>hello people</h3>
-      <ul>
-        <li>
-          <a href='#'>hello world</a>
-        </li>
-      </ul>
-    </React.Fragment>
-    // </div>
-  )
+    <div>
+      <Person/>
+      <Message/>
+    </div>
+  );
 }
-// function Greeting(){
-//   return(
-//     <div>
-//       <h1>hello World!!</h1>
-//     </div>
-//   )
-// }
 
-// React.createElement(element, props object, child)
-// const Greeting =() =>{
-//   return React.createElement('h1',{},'hello world')
-// }
+const Person = () =>{
+  return <h2>john doe</h2>
+}
+const Message = () =>{
+  return <p> this is my message</p>
+}
+
 
 ReactDom.render(<Greeting/>,document.getElementById('root'));
 
 
-// function App(){
-//   return(
-//     <di>
-//       <h3>hello world</h3>
-//       <button>click</button>
-//     </di>
-//   )
-// }
-
-// export default App
