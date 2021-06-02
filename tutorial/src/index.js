c70750eaede3c3a0012b6d99abbbd048062279b7
Chih-Chien-Lin <ro0566320@gmail.com>
2021-06-02 14:44:25 -0400

    dynamically use variable to render page

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -8,41 +8,22 @@ function BookList(){
   return(
     <section className='booklist'>
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      
 
     </section>
   );
 }
 
 
-
+const author = 'Elin Hilderbrand'
 const Book = () => {
+  const title = 'Golden Girl'
   return <article className='book'>
-    <Image />
-    <Title></Title>
-    <Author></Author>
+    <img src="https://images-na.ssl-images-amazon.com/images/I/81a5IRbOR4L._AC_UL200_SR200,200_.jpg" alt=""/>
+    <h1>{title}</h1>
+    <h4>{author.toUpperCase()}</h4>
   </article>
 }
 
-const Image = () => {
-  return <img src="https://images-na.ssl-images-amazon.com/images/I/81a5IRbOR4L._AC_UL200_SR200,200_.jpg" alt=""/>
-}
-
-const Title = () => {
-  return <h1>Golden Girl</h1>
-}
-
-const Author = () => {
-  // if we type style={}, everything inside of {} will be considered as JS
-  // if we type style={{}}, everything inside will be considered as object(css object)
-  return <h4 style={{color:'#617d98',fontSize:'0.75rem',marginTop:'0.5rem'}}>Elin Hilderbrand</h4>
-}
 
 ReactDom.render(<BookList/>,document.getElementById('root'));
 
