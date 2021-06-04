dd03644e70142ab18cbf643c904fe8f159ab3cf8
Chih-Chien-Lin <ro0566320@gmail.com>
2021-06-04 11:19:16 -0400

    props desctruction

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -10,30 +10,39 @@ const firstBook = {
   title: 'Golden Girl',
   author: 'Elin Hilderbrand'
 }
-
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/916I7ZLbwPL._AC_UL200_SR200,200_.jpg",
+  title: 'After the Fall: Being American in the World We\'ve Made',
+  author: 'Ben Rhodes'
+}
 
 function BookList(){
   return(
     <section className='booklist'>
       <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
-      <Book title='random title' number={22} author='hello'/>
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
 
-    </section>
+    </section> 
   );
 }
 
-
-// if use props, need to make sure all the variables are defined, or it'll be undefined error.
-const Book = (props) => {
-  console.log(props);
+// props destruction method1
+// const Book = (props) => {
+// const{img, title, author} = props
+// return <article className='book'>
+//     <img src={img} alt=""/>
+//     <h1>{title}</h1>
+//     <h4>{author.toUpperCase()}</h4>
+//   </article>
+// }
+//props desctruction method2
+const Book = ({img, title, author}) => {
   return <article className='book'>
-    <img src={props.img} alt=""/>
-    <h1>{props.title}</h1>
-    <h4>{props.author.toUpperCase()}</h4>
-    <p>{props.job}</p>
-    <p>{props.title}</p>
-  </article>
-}
+      <img src={img} alt=""/>
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+    </article>
+  }
 
 
 ReactDom.render(<BookList/>,document.getElementById('root'));
