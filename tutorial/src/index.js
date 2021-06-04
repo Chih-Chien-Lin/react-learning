a4d04b5b43892f8a11396b6b7e93e5fb0c4a0661
Chih-Chien-Lin <ro0566320@gmail.com>
2021-06-03 23:50:21 -0400

    change config

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -4,9 +4,18 @@ import ReactDom from 'react-dom'
 // CSS
 import './index.css'
 
+// Variables
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81a5IRbOR4L._AC_UL200_SR200,200_.jpg",
+  title: 'Golden Girl',
+  author: 'Elin Hilderbrand'
+}
+
+
 function BookList(){
   return(
     <section className='booklist'>
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
       <Book />
 
     </section>
@@ -14,13 +23,15 @@ function BookList(){
 }
 
 
-const author = 'Elin Hilderbrand'
-const Book = () => {
-  const title = 'Golden Girl'
+
+const Book = (props) => {
+  console.log(props);
   return <article className='book'>
-    <img src="https://images-na.ssl-images-amazon.com/images/I/81a5IRbOR4L._AC_UL200_SR200,200_.jpg" alt=""/>
+    <img src={img} alt=""/>
     <h1>{title}</h1>
     <h4>{author.toUpperCase()}</h4>
+    <p>{props.job}</p>
+    <p>{props.title}</p>
   </article>
 }
 
@@ -28,3 +39,4 @@ const Book = () => {
 ReactDom.render(<BookList/>,document.getElementById('root'));
 
 
+ 
